948d04380eb17e07daab534dc619aa67c165e89f
Bora Guven <guvenbe@gmail.com>
2019-05-09 19:58:54 -0400

    Single Reponsibility Principle

---
+++ src/com/activemesa/solid/srp/SingleResponsibilityPrinciple.java
@@ -0,0 +1,98 @@
+package com.activemesa.solid.srp;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+class Journal
+{
+    private final List<String> entries = new ArrayList<>();
+
+    private static int count = 0;
+
+    public void addEntry(String text)
+    {
+        entries.add("" + (++count) + ": " + text);
+    }
+
+    public void removeEntry(int index)
+    {
+        entries.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), entries);
+    }
+
+    // here we break SRP
+    public void save(String filename) throws Exception
+    {
+        try (PrintStream out = new PrintStream(filename))
+        {
+            out.println(toString());
+        }
+    }
+
+    public void load(String filename) {}
+    public void load(URL url) {}
+}
+
+// handles the responsibility of persisting objects
+class Persistence
+{
+    public void saveToFile(Journal journal,
+                           String filename, boolean overwrite) throws Exception
+    {
+        if (overwrite || new File(filename).exists())
+            try (PrintStream out = new PrintStream(filename)) {
+                out.println(journal.toString());
+            }
+    }
+
+    public void load(Journal journal, String filename) {}
+    public void load(Journal journal, URL url) {}
+}
+
+class SRPDemo
+{
+    public static void main(String[] args) throws Exception
+    {
+        Journal j = new Journal();
+        j.addEntry("I cried today");
+        j.addEntry("I ate a bug");
+        System.out.println(j);
+
+        String s = null;
+
+        Persistence p = new Persistence();
+        String filename = "/tmp/journal.txt";
+        p.saveToFile(j, filename, true);
+
+
+        Process proc=Runtime.getRuntime().exec("cat " + filename);
+//        Process proc=Runtime.getRuntime().exec("ls");
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+
+    }
+}
