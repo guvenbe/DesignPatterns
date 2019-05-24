097b6075e5d90c31da614496fbd631679f74df01
Bora Guven <guvenbe@gmail.com>
2019-05-24 00:14:27 -0400

    Factory

---
+++ src/com/activemesa/creational/factories/excercise/PersonFactory.java
@@ -0,0 +1,26 @@
+package com.activemesa.creational.factories.excercise;
+
+import static org.junit.Assert.assertEquals;
+
+class Person
+{
+    public int id;
+    public String name;
+
+    public Person(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class PersonFactory
+{
+    private int id = 0;
+
+    public Person createPerson(String name)
+    {
+        return new Person(id++, name);
+    }
+}
+
